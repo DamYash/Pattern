e240039598bbb8603e97076b77a0d474de56a78f
Yashpreet Kaur <80843222+DamYash@users.noreply.github.com>
2021-08-01 00:18:04 -0400

    Create PatternLikeRightAngledTriangle.java

---
+++ PatternLikeRightAngledTriangle.java
@@ -0,0 +1,23 @@
+public class PatternLikeRightAngledTriangle 
+{
+	
+	public static void main(String[] args) 
+	{
+		int i, j;
+		for (i=1; i<5; i++)
+		{
+			for (j=1; j<=i; j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}
+
+/** Output:
+1
+22
+333
+4444
+**/
